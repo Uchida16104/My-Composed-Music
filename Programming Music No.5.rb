ccd2aa938b49ec1665c89f9a64ca396ebdfbdf6e
HU <s1B101701526@toyo.jp>
2018-02-03 13:50:55 +0900

    Create Programming Music No.5.rb

---
+++ Programming Music No.5.rb
@@ -0,0 +1,234 @@
+with_fx :echo do
+  with_fx :reverb do
+    with_fx :echo do
+      with_fx :reverb do
+        with_fx :echo do
+          with_fx :reverb do
+            100.times do
+              use_bpm rrand(0,1000)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(0,231)
+              sleep rrand(0,1)
+              use_bpm rrand(0,1000)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(48,83)
+              sleep rrand(0,1)
+              use_bpm rrand(0,500)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(0,231)
+              sleep rrand(0,1)
+              use_bpm rrand(0,500)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(48,83)
+              sleep rrand(0,1)
+              use_bpm rrand(0,300)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(0,231)
+              sleep rrand(0,1)
+              use_bpm rrand(0,300)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(48,83)
+              sleep rrand(0,1)
+              use_bpm rrand(0,200)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(0,231)
+              sleep rrand(0,1)
+              use_bpm rrand(0,200)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(48,83)
+              sleep rrand(0,1)
+              use_bpm rrand(0,100)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(0,231)
+              sleep rrand(0,1)
+              use_bpm rrand(0,100)
+              use_synth :piano
+              use_synth :dull_bell
+              use_synth :pretty_bell
+              play sample :ambi_choir
+              play sample :ambi_piano
+              play sample :drum_bass_hard
+              play sample :drum_bass_soft
+              play sample :drum_cowbell
+              play sample :drum_snare_hard
+              play sample :drum_snare_soft
+              play sample :drum_tom_hi_hard
+              play sample :drum_tom_hi_soft
+              play sample :drum_tom_lo_hard
+              play sample :drum_tom_lo_soft
+              play sample :drum_tom_mid_hard
+              play sample :drum_tom_mid_soft
+              play sample :elec_bong
+              play sample :elec_chime
+              play sample :loop_tabla
+              play rrand(48,83)
+              sleep rrand(0,1)
+            end
+          end
+        end
+      end
+    end
+  end
+end
