f40a124666eb04c317a844f9172ea4704f9a1f84
HU <s1B101701526@toyo.jp>
2018-02-09 10:00:00 +0900

    Create Programming Music No.8.rb

---
+++ Programming Music No.8.rb
@@ -0,0 +1,301 @@
+10.times do
+  with_fx :echo do
+    with_fx :reverb do
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  with_fx :echo do
+                    with_fx :reverb do
+                      use_synth :piano
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :loop_tabla
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_dhec
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_ghe1
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ghe2
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ghe3
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_ghe4
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_ghe5
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ghe6
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ghe7
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_ghe8
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_ke1
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ke2
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_ke3
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_na
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_na_o
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_na_s
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_re
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_tas1
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_tas2
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_tas3
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_te1
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_te2
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_te_m
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_te_ne
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_e_fifths
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :tabla_tun1
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_tun2
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_e_slide
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :tabla_tun3
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :guit_em9
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,1000)
+                      play sample :guit_harmonics
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                      use_bpm rrand(0,500)
+                      play sample :elec_bong
+                      play rrand(0,231)
+                      play rrand(48,83)
+                      sleep rrand(0,1)
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
