45cbb33d06821806737513c144b3ea9f71932067
HU <s1B101701526@toyo.jp>
2018-02-13 19:51:19 +0900

    Create Programming Music No.9.rb

---
+++ Programming Music No.9.rb
@@ -0,0 +1,429 @@
+100.times do
+  in_thread do
+    200.times do
+      use_bpm rrand(0,1000)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  with_fx :echo do
+                    with_fx :reverb do
+                      with_fx :echo do
+                        with_fx :reverb do
+                          sample :accordion
+                          sleep rrand(0,1)
+                          sample :bagpipe
+                          sleep rrand(0,1)
+                          sample :cello
+                          sleep rrand(0,1)
+                          sample :drum_bass_hard
+                          sleep rrand(0,1)
+                          sample :elec_beep
+                          sleep rrand(0,1)
+                          sample :fiddle
+                          sleep rrand(0,1)
+                          sample :gamlan1
+                          sleep rrand(0,1)
+                          sample :handdrum
+                          sleep rrand(0,1)
+                          sample :indoperc1
+                          sleep rrand(0,1)
+                          sample :kalimba
+                          sleep rrand(0,1)
+                          sample :lacote
+                          sleep rrand(0,1)
+                          sample :marimba
+                          sleep rrand(0,1)
+                          sample :nakers
+                          sleep rrand(0,1)
+                          sample :oboe
+                          sleep rrand(0,1)
+                          sample :perc_bell
+                          sleep rrand(0,1)
+                          sample :rackett
+                          sleep rrand(0,1)
+                          sample :sackbut
+                          sleep rrand(0,1)
+                          sample :tabla1
+                          sleep rrand(0,1)
+                          sample :vibla
+                          sleep rrand(0,1)
+                          sample :woodblock1
+                          sleep rrand(0,1)
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    180.times do
+      use_bpm rrand(0,900)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  with_fx :echo do
+                    with_fx :reverb do
+                      with_fx :echo do
+                        sample :afrharpmut
+                        sleep rrand(0,1)
+                        sample :bandi
+                        sleep rrand(0,1)
+                        sample :chalumeau
+                        sleep rrand(0,1)
+                        sample :drum_bass_soft
+                        sleep rrand(0,1)
+                        sample :elec_bell
+                        sleep rrand(0,1)
+                        sample :flute
+                        sleep rrand(0,1)
+                        sample :ghostbreath1
+                        sleep rrand(0,1)
+                        sample :harp
+                        sleep rrand(0,1)
+                        sample :indoperc2
+                        sleep rrand(0,1)
+                        sample :kayap
+                        sleep rrand(0,1)
+                        sample :loop_3d_printer
+                        sleep rrand(0,1)
+                        sample :mehackit_phone1
+                        sleep rrand(0,1)
+                        sample :oh
+                        sleep rrand(0,1)
+                        sample :perc_bell2
+                        sleep rrand(0,1)
+                        sample :rauschpfeife
+                        sleep rrand(0,1)
+                        sample :sax
+                        sleep rrand(0,1)
+                        sample :tabla2
+                        sleep rrand(0,1)
+                        sample :vinyl_backspin
+                        sleep rrand(0,1)
+                        sample :woodblock2
+                        sleep rrand(0,1)
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    160.times do
+      use_bpm rrand(0,800)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  with_fx :echo do
+                    with_fx :reverb do
+                      sample :ah
+                      sleep rrand(0,1)
+                      sample :banjo
+                      sleep rrand(0,1)
+                      sample :clappers
+                      sleep rrand(0,1)
+                      sample :drum_cowbell
+                      sleep rrand(0,1)
+                      sample :elec_blip
+                      sleep rrand(0,1)
+                      sample :flutter1
+                      sleep rrand(0,1)
+                      sample :ghostbreath2
+                      sleep rrand(0,1)
+                      sample :harpsichord
+                      sleep rrand(0,1)
+                      sample :korantalk2
+                      sleep rrand(0,1)
+                      sample :loop_amen
+                      sleep rrand(0,1)
+                      sample :mehackit_phone2
+                      sleep rrand(0,1)
+                      sample :ohit
+                      sleep rrand(0,1)
+                      sample :perc_door
+                      sleep rrand(0,1)
+                      sample :rebec
+                      sleep rrand(0,1)
+                      sample :saxophone
+                      sleep rrand(0,1)
+                      sample :tabla3
+                      sleep rrand(0,1)
+                      sample :vinyl_hiss
+                      sleep rrand(0,1)
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    140.times do
+      use_bpm rrand(0,700)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  with_fx :echo do
+                    sample :ambi_choir
+                    sleep rrand(0,1)
+                    sample :bass_dnb_f
+                    sleep rrand(0,1)
+                    sample :clarinet
+                    sleep rrand(0,1)
+                    sample :drum_cymbal_closed
+                    sleep rrand(0,1)
+                    sample :elec_blip2
+                    sleep rrand(0,1)
+                    sample :flutter2
+                    sleep rrand(0,1)
+                    sample :ghostlaugh
+                    sleep rrand(0,1)
+                    sample :horn1
+                    sleep rrand(0,1)
+                    sample :koreancymb
+                    sleep rrand(0,1)
+                    sample :loop_amen_full
+                    sleep rrand(0,1)
+                    sample :mehackit_phone3
+                    sleep rrand(0,1)
+                    sample :organ
+                    sleep rrand(0,1)
+                    sample :perc_impact1
+                    sleep rrand(0,1)
+                    sample :recorder
+                    sleep rrand(0,1)
+                    sample :schalmei
+                    sleep rrand(0,1)
+                    sample :tabla_dhec
+                    sleep rrand(0,1)
+                    sample :vinyl_rewind
+                    sleep rrand(0,1)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    120.times do
+      use_bpm rrand(0,600)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                with_fx :reverb do
+                  sample :ambi_dark_woosh
+                  sleep rrand(0,1)
+                  sample :bass_drop_c
+                  sleep rrand(0,1)
+                  sample :clavichord
+                  sleep rrand(0,1)
+                  sample :drum_cymbal_hard
+                  sleep rrand(0,1)
+                  sample :elec_blup
+                  sleep rrand(0,1)
+                  sample :fortepiano
+                  sleep rrand(0,1)
+                  sample :glitch_bass_g
+                  sleep rrand(0,1)
+                  sample :horn2
+                  sleep rrand(0,1)
+                  sample :koreantalk1
+                  sleep rrand(0,1)
+                  sample :loop_breakbeat
+                  sleep rrand(0,1)
+                  sample :mehackit_phone4
+                  sleep rrand(0,1)
+                  sample :perc_impact2
+                  sleep rrand(0,1)
+                  sample :regal
+                  sleep rrand(0,1)
+                  sample :seierm
+                  sleep rrand(0,1)
+                  sample :tabla_ghe1
+                  sleep rrand(0,1)
+                  sample :vinyl_scratch
+                  sleep rrand(0,1)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    100.times do
+      use_bpm rrand(0,500)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              with_fx :echo do
+                sample :ambi_drone
+                sleep rrand(0,1)
+                sample :bass_hard_c
+                sleep rrand(0,1)
+                sample :com01
+                sleep rrand(0,1)
+                sample :drum_cymbal_open
+                sleep rrand(0,1)
+                sample :elec_bong
+                sleep rrand(0,1)
+                sample :fue
+                sleep rrand(0,1)
+                sample :glitch_perc1
+                sleep rrand(0,1)
+                sample :koto
+                sleep rrand(0,1)
+                sample :loop_compus
+                sleep rrand(0,1)
+                sample :mehackit_robot1
+                sleep rrand(0,1)
+                sample :perc_snap
+                sleep rrand(0,1)
+                sample :roar1
+                sleep rrand(0,1)
+                sample :shakuhachi
+                sleep rrand(0,1)
+                sample :tabla_ghe2
+                sleep rrand(0,1)
+                sample :violin
+                sleep rrand(0,1)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    80.times do
+      use_bpm rrand(0,400)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            with_fx :reverb do
+              sample :ambi_glass_hum
+              sleep rrand(0,1)
+              sample :bass_hit_c
+              sleep rrand(0,1)
+              sample :com02
+              sleep rrand(0,1)
+              sample :drum_cymbal_pedal
+              sleep rrand(0,1)
+              sample :elec_chime
+              sleep rrand(0,1)
+              sample :glitch_perc2
+              sleep rrand(0,1)
+              sample :loop_drone_g_97
+              sleep rrand(0,1)
+              sample :mehackit_robot2
+              sleep rrand(0,1)
+              sample :perc_snap2
+              sleep rrand(0,1)
+              sample :shamisen
+              sleep rrand(0,1)
+              sample :tabla_ghe3
+              sleep rrand(0,1)
+            end
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    60.times do
+      use_bpm rrand(0,300)
+      with_fx :echo do
+        with_fx :reverb do
+          with_fx :echo do
+            sample :ambi_glass_rub
+            sleep rrand(0,1)
+            sample :bass_thick_c
+            sleep rrand(0,1)
+            sample :com03
+            sleep rrand(0,1)
+            sample :drum_cymbal_soft
+            sleep rrand(0,1)
+            sample :elec_cymbal
+            sleep rrand(0,1)
+            sample :glitch_perc3
+            sleep rrand(0,1)
+            sample :loop_electric
+            sleep rrand(0,1)
+            sample :mehackit_robot3
+            sleep rrand(0,1)
+            sample :perc_swash
+            sleep rrand(0,1)
+            sample :shawm
+            sleep rrand(0,1)
+            sample :tabla_ghe4
+            sleep rrand(0,1)
+          end
+        end
+      end
+    end
+  end
+  in_thread do
+    40.times do
+      use_bpm rrand(0,200)
+      with_fx :echo do
+        with_fx :reverb do
+          sample :ambi_haunted_hum
+          sleep rrand(0,1)
+          sample :bass_trance_c
+          sleep rrand(0,1)
+          sample :contrabass
+          sleep rrand(0,1)
+          sample :drum_heavy_kick
+          sleep rrand(0,1)
+          sample :elec_filt_snare
+          sleep rrand(0,1)
+          sample :glitch_perc4
+          sleep rrand(0,1)
+          sample :loop_garzul
+          sleep rrand(0,1)
+          sample :mehackit_robot4
+          sleep rrand(0,1)
+          sample :perc_swoosh
+          sleep rrand(0,1)
+          sample :shenai
+          sleep rrand(0,1)
+          sample :tabla_ghe5
+          sleep rrand(0,1)
+        end
+      end
+    end
+  end
+end
