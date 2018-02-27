e44b0bdce282ad1c58ba1225a2aad4bb7fe7c137
HU <s1B101701526@toyo.jp>
2018-02-27 15:38:49 +0900

    Create Mixed Music No.1.rb

---
+++ Mixed Music No.1.rb
@@ -0,0 +1,446 @@
+use_bpm rrand(0,100)
+in_thread do
+  100.times do
+    sample :microtone_quarter_c1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_c2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_c3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_c4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_cs1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_cs2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_cs3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_cs4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_d1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_d2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_d3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_d4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_eb1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_eb2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_eb3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_eb4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_e1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_e2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_e3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_e4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_f1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_f2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_f3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_f4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_fs1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_fs2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_fs3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_fs4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_g1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_g2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_g3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_g4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_gs4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_a4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_bb1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_bb2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_bb3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_bb4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_b1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_b2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_b3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_b4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_chord_c1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :microtone_quarter_chord_c2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_all1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_all2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_all3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_all4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_bs1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_bs2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_bs3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_bs4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_nor1
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_nor2
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_nor3
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_nor4
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_origin_all
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_origin_bs
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    sample :cluster_microtone_origin_nor
+    sleep rrand(0,10)
+  end
+end
+in_thread do
+  100.times do
+    use_synth :piano
+    play rrand(60,72)
+    sleep rrand(0,10)
+  end
+end
